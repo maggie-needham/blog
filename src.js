f9531a7a457199d0b68f796c18506a721335c591
maggie-needham <maggieneedham01@gmail.com>
2025-12-12 23:37:14 -0500

    successfully spun

--- src.js
+++ src.js
@@ -1,7 +1,17 @@
-// function spin(name) {
-//     console.log('in here')
-//     var blog = document.getElementsByClassName(name)[0];
-//     // reset
-//     blog.style.animationName = "none";
-//     blog.style.animationName = "spin";
-// }
+// animation for "blog" 
+const keyframes = [
+    { transform: 'rotate(0deg)'},
+    { transform: 'rotate(360deg)'}
+]
+
+const options = {
+    duration: 2000,
+    iterations: 1,
+}
+
+// makes "blog" spin
+function spin(name) {
+    console.log('in here')
+    var blog = document.getElementsByClassName(name)[0];
+    const animation = blog.animate(keyframes, options);
+}
